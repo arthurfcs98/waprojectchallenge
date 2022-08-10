14bd92c6f186e5e93daecb2842f4c05f0d35d856
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-08-10 17:39:24 -0300

    add prettier, eslint, jest and ts configurations

--- wachallengeApi/src/server.ts
+++ wachallengeApi/src/server.ts
@@ -1,15 +1,15 @@
-import cors from "cors";
-import express from "express";
-import "express-async-error";
-import "reflect-metadata";
+import cors from 'cors';
+import express from 'express';
+import 'express-async-error';
+import 'reflect-metadata';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World!' });
 });
 
-app.listen(5001, () => console.log("Server is running on port 5001"));
+app.listen(5001, () => console.log('Server is running on port 5001'));
