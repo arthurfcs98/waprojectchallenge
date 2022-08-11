5810dbbb26d503694afe6172dca7a3577f40400e
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-08-11 19:43:02 -0300

    add get rout do list movies

--- wachallengeApi/src/routes/index.ts
+++ wachallengeApi/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { refreshMoviesUseCase } from '../modules/movies/useCases/refreshMovies';
+import { ListMoviesController } from '../modules/movies/useCases/listMovies/ListMoviesController';
 import { RefreshMoviesController } from '../modules/movies/useCases/refreshMovies/RefreshMoviesController';
 
 const router = Router();
 
 const refreshMoviesController = new RefreshMoviesController();
+const listMoviesController = new ListMoviesController();
 router.post('/', refreshMoviesController.handle);
+router.get('/', listMoviesController.handle);
 
 export { router };
