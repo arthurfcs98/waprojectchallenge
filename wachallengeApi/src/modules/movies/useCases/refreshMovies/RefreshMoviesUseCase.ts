f2c2e531dd55698817f6e9b7783be3139b26c225
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-08-11 23:09:57 -0300

    add poster path url

--- wachallengeApi/src/modules/movies/useCases/refreshMovies/RefreshMoviesUseCase.ts
+++ wachallengeApi/src/modules/movies/useCases/refreshMovies/RefreshMoviesUseCase.ts
@@ -8,7 +8,10 @@ class RefreshMoviesUseCase {
 
     async execute(refresh_page: number) {
         if (refresh_page > 500)
-            throw new AppError('page must be less than or equal to 500', 422);
+            throw new AppError(
+                'refresh_page must be less than or equal to 500',
+                422,
+            );
 
         const { data: responseData1 } = await movieApi.get('/movie/popular', {
             params: {
@@ -27,7 +30,10 @@ class RefreshMoviesUseCase {
             ...(responseData2.results as Movie[]),
         ];
 
-        movies.forEach(movie => delete movie.id);
+        movies.forEach(movie => {
+            movie.poster_path = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+            delete movie.id;
+        });
 
         await this.moviesRepository.clearMoviesTable();
         await this.moviesRepository.InsertMoviesByArray(movies);
